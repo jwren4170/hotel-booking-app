26166864a81a0343150f9fccf5a151e11e262e86
jwren4170 <jwren4170@att.net>
2026-05-12 20:25:11 -0500

    installed mongosse and onnected to db

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,18 @@
 import express, { type Express } from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
+
+mongoose
+	.connect(process.env.DATABASE_URI as string)
+	.then(() => {
+		console.log("DB connected successfully");
+	})
+	.catch((e) => {
+		console.log(`Something went wrong ${e.message}`);
+	})
+	.finally(() => {
+		console.log("Nodemon is listening for changes");
+	});
 
 const app: Express = express();
 const PORT = 3000;
